c792fc365daee1b9eda94a1784208b3055d74ddd
aidanaden <aidanaden@hotmail.com>
2022-11-22 16:19:56 +0800

    chore: disable ssr

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import dotenv from "dotenv";
 import { defineConfig } from "vite";
 // @ts-expect-error no typing
 import vercel from "solid-start-vercel";
-  
+
 export default defineConfig(() => {
   dotenv.config();
   return {
-    plugins: [solid({ ssr: true, adapter: vercel({ edge: false }) })],
+    plugins: [solid({ ssr: false, adapter: vercel({ edge: false }) })],
   };
 });
-  
